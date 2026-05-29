6de7f0d04047410ff170e0b9df91896b0647fc7a
Vasilina Antipova <vasilinaantipova@gmail.com>
2026-05-29 11:08:10 +0300

    FEAT: реализация интерфейса приложения

---
+++ mainwindow.cpp
@@ -0,0 +1,123 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QThread>
+#include <QTimer>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pbtnStartPath_clicked()
+{
+    startPath = "";
+    getPath();
+}
+
+
+void MainWindow::on_pbtnFinishPath_clicked()
+{
+    finishPath = "";
+    getPath();
+}
+
+void MainWindow::getPath() {
+    QString path = QStandardPaths::writableLocation(QStandardPaths::PicturesLocation);
+    if (startPath.isEmpty()) {
+        startPath = QFileDialog::getExistingDirectory(this, "Выберите нужный каталог", path);
+        finishPath = "";
+        ui->leFinishPath->setText(finishPath);
+        ui->leStartPath->setText(startPath);
+    }
+    else if (finishPath.isEmpty()){
+        finishPath = QFileDialog::getExistingDirectory(this, "Выберите нужный каталог", path);
+        while(startPath == finishPath) {
+            QMessageBox::warning(this, "Ошибка!", "Невозможно сохранить фотографии в тот же каталог, "
+                                                  "в котором они расположены изначально! "
+                                                  "Выберите другой каталог.");
+            finishPath = QFileDialog::getExistingDirectory(this, "Выберите нужный каталог", path);
+        }
+        ui->leFinishPath->setText(finishPath);
+    }
+}
+
+void MainWindow::on_spCntManyThreads_valueChanged(int arg1)
+{
+    cntThreads = arg1;
+}
+
+
+void MainWindow::on_pbtnFinish_clicked()
+{
+    ui->pbtnStartThreads->setEnabled(true);
+    ui->pbtnStart->setEnabled(true);
+
+    ui->pbtnFinish->setEnabled(false);
+    ui->pbtnStop->setEnabled(false);
+
+    // здесь будет функция прерывания потока
+}
+
+
+void MainWindow::on_pbtnStop_clicked()
+{
+    // здесь будет функция остановки потока
+}
+
+
+void MainWindow::on_pbtnStart_clicked()
+{
+    ui->pbtnStartThreads->setEnabled(false);
+    ui->pbtnFinishThreads->setEnabled(false);
+    ui->pbtnStopThreads->setEnabled(false);
+
+    ui->pbtnStart->setEnabled(false);
+    ui->pbtnFinish->setEnabled(true);
+    ui->pbtnStop->setEnabled(true);
+
+    // здесь будет функция запуска потока
+}
+
+
+void MainWindow::on_pbtnFinishThreads_clicked()
+{
+    ui->pbtnStartThreads->setEnabled(true);
+    ui->pbtnStart->setEnabled(true);
+
+    ui->pbtnFinishThreads->setEnabled(false);
+    ui->pbtnStopThreads->setEnabled(false);
+
+    // здесь будет функция прерывания потоков
+}
+
+
+void MainWindow::on_pbtnStopThreads_clicked()
+{
+    // здесь будет функция остановки потоков
+}
+
+
+void MainWindow::on_pbtnStartThreads_clicked()
+{
+    ui->pbtnStart->setEnabled(false);
+    ui->pbtnFinish->setEnabled(false);
+    ui->pbtnStop->setEnabled(false);
+
+    ui->pbtnStartThreads->setEnabled(false);
+    ui->pbtnFinishThreads->setEnabled(true);
+    ui->pbtnStopThreads->setEnabled(true);
+
+    // здесб будет функция запсуков потоков
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pbtnStartPath_clicked();
+    void on_pbtnFinishPath_clicked();
+
+    void on_spCntManyThreads_valueChanged(int arg1);
+
+private:
+    Ui::MainWindow *ui;
+
+    QString startPath = "";
+    QString finishPath = "";
+    int cntThreads = 0;
+
+    void getPath();
+
+};
+#endif // MAINWINDOW_H
